94ef6bf06bc2d624f34e29e963333bb4b178068e
Jair Augusto Dério Boeing <jairderioboeing@gmail.com>
2023-12-01 11:09:44 -0300

    commit update

--- src/Principal.java
+++ src/Principal.java
@@ -2,21 +2,36 @@
 public class Principal {
 
 	public static void main(String[] args) {
-		Menu.Menu();
-		switch (Menu.Menu()) {
-
-		case 1:
-
-			nomearCtt.contato();
-			numeroDoCtt.numero();
-			break;
-
-		case 2:
-
-			break;
-
+		while(true) {
+			switch (Menu.Menu()) {
+	
+			case 1:
+	
+				nomearCtt.contato();
+				numeroDoCtt.numero();
+				break;
+			
+			case 2:
+				
+				break;
+				
+			case 3:
+				
+				break;
+				
+			case 4:
+				
+				break;
+				
+			case 0:
+				System.out.println("Fim Do Algoritmos.");
+				break;
+				
+			default:
+				System.out.println("Opção Inválida.");
+				break;
+			}
 		}
-
 	}
 
 }
