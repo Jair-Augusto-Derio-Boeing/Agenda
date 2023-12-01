ba79a61bf75cedb5c6696b9856f0de297b876b91
Jair Augusto Dério Boeing <jairderioboeing@gmail.com>
2023-12-01 09:28:21 -0300

    commit limpo
    limpei o cod para começarmos a trabalhar

--- src/Principal.java
+++ src/Principal.java
@@ -2,12 +2,7 @@
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Dani Linda");
-		String teste = "De Versionamento";
-		System.out.println("COntinuando o teste " + teste);
-//		oi AEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
 		
-		System.out.println("el nino");
 	}
 
 }
