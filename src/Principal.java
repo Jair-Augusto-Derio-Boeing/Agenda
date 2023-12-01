71ce682c012ecb29b577db2f406f96a64ae4f398
Jair Augusto Dério Boeing <jairderioboeing@gmail.com>
2023-12-01 09:23:21 -0300

    coommit teste1

--- src/Principal.java
+++ src/Principal.java
@@ -3,6 +3,8 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Dani Linda");
+		String teste = "De Versionamento";
+		System.out.println("COntinuando o teste " + teste);
 //		oi AEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
 	}
 
