065623a324dc042754f7ee1f6192794cface9f94
João Valério Santana <ohomiebrabo2307@gmail.com>
2023-12-01 09:23:45 -0300

    commit teste 2
    criado 2 syso

--- src/Principal.java
+++ src/Principal.java
@@ -4,6 +4,8 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Dani Linda");
 //		oi AEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+		
+		System.out.println("el nino");
 	}
 
 }
