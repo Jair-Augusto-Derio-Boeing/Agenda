3d2738b53eecd97a728e2d576074c854e2a0e48d
Jair Augusto Dério Boeing <jairderioboeing@gmail.com>
2023-12-01 09:58:40 -0300

    commit update

---
+++ src/Menu.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Menu {
+	public static int Menu() {
+		Scanner t = new Scanner(System.in);
+		while(true) {
+			System.out.println("1ª Opção - Adicionar Contato.");
+			System.out.println("2ª Opção - Listar Contatos.");
+			System.out.println("3ª Opção - Buscar Contato.");
+			System.out.println("4ª Opção - Remover Contato.");
+			System.out.println("Digite 0 - Para Finalizar.");
+			int escolha = t.nextInt();
+			return escolha;
+		}
+	}
+}
