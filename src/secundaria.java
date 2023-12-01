a7b4e02ba7fe738f10c7e6a022eff798c95caedd
João Valério Santana <ohomiebrabo2307@gmail.com>
2023-12-01 09:26:51 -0300

    commit teste3
    criando nova class

--- src/secundaria.java
+++ src/secundaria.java
@@ -1,4 +1,11 @@
 
 public class secundaria {
+	public static void main(String[] args) {
+		System.out.println("Dani Linda");
+//		oi AEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+		
+		System.out.println("yo soy big");
+	}
 
 }
+
